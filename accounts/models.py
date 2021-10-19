08988ab86e2b1f9a7bab56035d99ff4b7c676a55
Stephanie Cushing <slcushing@gmail.com>
2021-10-18 20:44:04 -0400

    initial commit

---
+++ accounts/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.conf import settings
+
+
+# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='profiles', null=True)
+    alias = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return self.user.username
---
+++ frontend/static/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      I'm a news app.
+    </div>
+  );
+}
+
+export default App;
