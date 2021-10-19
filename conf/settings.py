694c45300f4aa3bf5f8accddb105f6cb4cdb55f4
Stephanie Cushing <slcushing@gmail.com>
2021-10-18 22:06:42 -0400

    gitignore

--- conf/settings.py
+++ conf/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^n17c2en2o3z74v&ii3olx-*l5c-k&g_fi4(880*%nwa=kizj5'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- frontend/static/src/App.js
+++ frontend/static/src/App.js
@@ -4,7 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      I'm a news app.
+      <header className="header">
+        <h1>NEWS APP</h1>
+        <nav>
+          <ul className="nav-bar">
+            <li>World</li>
+            <li>U.S.</li>
+            <li>Sports</li>
+            <li>Tech</li>
+            <li>Opinion</li>
+          </ul>
+        </nav>
+
+      </header>
     </div>
   );
 }
